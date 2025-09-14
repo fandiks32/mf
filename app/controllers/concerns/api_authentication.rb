860f00caf6ecd739e8b622c88282dda598a318b6
Irfandi Achmad <irfandi.achmad@mekari.com>
2025-09-14 10:17:24 +0700

    fix error auth

--- app/controllers/concerns/api_authentication.rb
+++ app/controllers/concerns/api_authentication.rb
@@ -6,21 +6,32 @@ module ApiAuthentication
   def authenticate_user
     authenticate_with_http_basic do |user_id, password|
       @extracted_user_id = user_id
+      
+      # First check if user exists in the context of the current request
+      # For GET /users/{user_id}, the user being requested should exist
+      # For other endpoints, we need to authenticate the requesting user
+      
       @current_user = User.find_by(user_id: user_id)
 
       if @current_user.nil?
-        # User doesn't exist - should return 404 instead of 401
-        Rails.logger.warn({
-          event: "user_not_found",
-          controller: controller_name,
-          action: action_name,
-          user_id: user_id,
-          ip: request.remote_ip,
-          user_agent: request.user_agent,
-          timestamp: Time.current
-        }.to_json)
-        
-        raise ApiExceptions::UserNotFoundError.new
+        # User doesn't exist - should return 404 for GET requests, 401 for others
+        if action_name == 'show' && params[:user_id] == user_id
+          # GET /users/{user_id} with non-existent user = 404
+          Rails.logger.warn({
+            event: "user_not_found",
+            controller: controller_name,
+            action: action_name,
+            user_id: user_id,
+            ip: request.remote_ip,
+            user_agent: request.user_agent,
+            timestamp: Time.current
+          }.to_json)
+          raise ApiExceptions::UserNotFoundError.new
+        else
+          # Other endpoints with non-existent user = 401
+          render_authentication_error
+          false
+        end
       elsif @current_user.authenticate(password)
         Rails.logger.info({
           event: "authentication_success",
@@ -33,6 +44,7 @@ module ApiAuthentication
         
         @current_user
       else
+        # User exists but wrong password = 401
         render_authentication_error
         false
       end
